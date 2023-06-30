1c0d21266df4bfd338fbe2722f9ad971aad918a9
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-06-30 12:19:01 +0300

    added toast and sing in functionality

---
+++ src/app/_providers/ToasterProvider/ui/ToasterProvider.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useToast } from '@/shared/lib/hooks';
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from '@/shared/ui/Toast/Toast';
+
+export function ToasterProvider() {
+  const { toasts } = useToast();
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        );
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,6 @@
 import { Icons } from './ui/Icons/Icons';
 import { Button } from './ui/Button/Button';
 import Spinner from './ui/Spinner/Spinner';
+import { Toast } from './ui/Toast/Toast';
 
-export { Spinner, Button, Icons };
+export { Spinner, Button, Icons, Toast };
