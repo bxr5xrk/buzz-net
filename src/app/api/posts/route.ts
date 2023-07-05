716cc43052b8eccd8af559b72d159d60d7743ba5
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-07-05 16:26:08 +0300

    added working infinite scrolling

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,75 @@
+import { getAuthSession } from '@/shared/lib/auth/auth';
+import { db } from '@/shared/lib/db/db';
+import { z } from 'zod';
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+
+  const session = await getAuthSession();
+
+  let followedCommunitiesIds: string[] = [];
+
+  if (session) {
+    const followedCommunities = await db.subscription.findMany({
+      where: {
+        userId: session.user.id
+      },
+      include: {
+        community: true
+      }
+    });
+
+    followedCommunitiesIds = followedCommunities.map((sub) => sub.community.id);
+  }
+
+  try {
+    const { limit, page, communityName } = z
+      .object({
+        limit: z.string(),
+        page: z.string(),
+        communityName: z.string().nullish().optional()
+      })
+      .parse({
+        communityName: url.searchParams.get('communityName'),
+        limit: url.searchParams.get('limit'),
+        page: url.searchParams.get('page')
+      });
+
+    let whereClause = {};
+
+    if (communityName) {
+      whereClause = {
+        community: {
+          name: communityName
+        }
+      };
+    } else if (session) {
+      whereClause = {
+        community: {
+          id: {
+            in: followedCommunitiesIds
+          }
+        }
+      };
+    }
+
+    const posts = await db.post.findMany({
+      take: parseInt(limit),
+      skip: (parseInt(page) - 1) * parseInt(limit), // skip should start from 0 for page 1
+      orderBy: {
+        createdAt: 'desc'
+      },
+      include: {
+        community: true,
+        votes: true,
+        author: true,
+        comments: true
+      },
+      where: whereClause
+    });
+
+    return new Response(JSON.stringify(posts));
+  } catch (error) {
+    return new Response('Could not fetch posts', { status: 500 });
+  }
+}
